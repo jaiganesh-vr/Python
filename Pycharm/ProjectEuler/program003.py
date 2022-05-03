008196d9781a40dbc92b5737e7bb39da23b36068
jaiganesh-vr <jaigan.pdy@gmail.com>
2022-05-02 20:11:54 -0400

    Added Project Euler

---
+++ Pycharm/ProjectEuler/program003.py
@@ -0,0 +1,31 @@
+def sqrt(x):
+    assert x >= 0
+    i = 1
+    while i * i <= x:
+        i *= 2
+    y = 0
+    while i > 0:
+        if (y + i)**2 <= x:
+            y += i
+        i //= 2
+    return y
+
+def compute():
+    n = 600851475143
+    while True:
+        p = least_prime_factor(n)
+        if p < n:
+            n //= p
+        else:
+            return str(n)
+
+def least_prime_factor(n):
+    assert n >= 2
+    for i in range(2,sqrt(n) + 1):
+        if n % i == 0:
+            return i
+    return n
+
+
+if __name__ == "__main__":
+	print(compute())
