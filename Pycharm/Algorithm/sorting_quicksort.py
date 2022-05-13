1988b53fc839a20e06b5541d51d1c1e8ca3f0a6a
jaiganesh-vr <jaigan.pdy@gmail.com>
2022-05-12 20:09:44 -0400

    Update sorting_quicksort.py

--- Pycharm/Algorithm/sorting_quicksort.py
+++ Pycharm/Algorithm/sorting_quicksort.py
@@ -10,12 +10,11 @@ def partition(array, low, high):
     # compare each element with pivot
     for j in range(low, high):
         if array[j] <= pivot:
-        # If element smaller than pivot is found
-        # swap it with the greater element pointed by i
-        i = i + 1
-
-        # Swapping element at i with element at j
-        (array[i], array[j]) = (array[j], array[i])
+         # If element smaller than pivot is found
+         # swap it with the greater element pointed by i
+         i = i + 1
+         # Swapping element at i with element at j
+         (array[i], array[j]) = (array[j], array[i])
 
     # Swap the pivot element with the greater element specified by i
     (array[i + 1], array[high]) = (array[high], array[i + 1])
