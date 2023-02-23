e9e009566b90f06c1d48e497a5f1c03e5b29704f
jaiganesh-vr <jaigan.pdy@gmail.com>
2023-02-23 11:35:01 -0500

    Refactored files

---
+++ Algorithm/Sorting/insertion.py
@@ -0,0 +1,25 @@
+def insertionSort(arr):
+    # Traverse through 1 to len(arr)
+    for i in range(1, len(arr)):
+        key = arr[i]
+        # Move elements of arr[0..i-1], that are
+        # greater than key, to one position ahead
+        # of their current position
+        j = i - 1
+        while j >= 0 and key < arr[j]:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+
+def printList(arr):
+    for i in range(len(arr)):
+        print(arr[i], end=" ")
+    print()
+
+if __name__ == '__main__':
+    arr = [12, 11, 13, 5, 6, 7]
+    print("Given array is", end="\n")
+    printList(arr)
+    insertionSort(arr)
+    print("Sorted array is: ", end="\n")
+    printList(arr)
