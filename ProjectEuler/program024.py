d06030c2d0cd8108fb818425e62ba9006a4c50df
jaiganesh-vr <jaigan.pdy@gmail.com>
2023-05-05 21:57:38 -0400

    Create program024.py

---
+++ ProjectEuler/program024.py
@@ -0,0 +1,10 @@
+import itertools
+
+def compute():
+	arr = list(range(10))
+	temp = itertools.islice(itertools.permutations(arr), 999999, None)
+	return "".join(str(x) for x in next(temp))
+
+
+if __name__ == "__main__":
+	print(compute())
