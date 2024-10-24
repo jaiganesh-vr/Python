4e2f7e6fda34308bff83f3461db8564fad2a913e
Jaiganesh <jaigan.pdy@gmail.com>
2024-10-24 17:57:00 +0530

    Update program078.py

--- ProjectEuler/program078.py
+++ ProjectEuler/program078.py
@@ -1,44 +1,41 @@
 import itertools
-def sqrt(x):
-    assert x >= 0
-    i = 1
-    while i * i <= x:
-        i *= 2
-    y = 0
-    while i > 0:
-        if (y + i) ** 2 <= x:
-            y += i
-        i //= 2
-    return y
-def is_prime(x):
-    if x <= 1:
-        return False
-    elif x <= 3:
-        return True
-    elif x % 2 == 0:
-        return False
-    else:
-        for i in range(3, sqrt(x) + 1, 2):
-            if x % i == 0:
-                return False
-        return True
+
+MODULUS = 10 ** 6
+
+
 def compute():
-    cond = lambda n: num_prime_sum_ways(n) > 5000
-    ans = next(filter(cond, itertools.count(2)))
-    return str(ans)
+    partitions = [1]
+    for i in itertools.count(len(partitions)):
+        # We calculate partitions[i] mod 10^6 using a formula based on generalized pentagonal numbers:
+        #   partitions(i) =   partitions(i - pentagonal(1)) + partitions(i - pentagonal(-1))
+        #                   - partitions(i - pentagonal(2)) - partitions(i - pentagonal(-2))
+        #                   + partitions(i - pentagonal(3)) + partitions(i - pentagonal(-3))
+        #                   - partitions(i - pentagonal(4)) - partitions(i - pentagonal(-4))
+        #                   + ...,
+        #   where pentagonal(j) = (3*n^2 - n) / 2, and
+        #   we stop the sum when i - pentagonal(+/-j) < 0.
+        # Note that for j > 0, pentagonal(j) < pentagonal(-j) < pentagonal(j+1).
+        #
+        # (The formula is used without mathematical justification;
+        # see https://en.wikipedia.org/wiki/Partition_(number_theory)#Generating_function .)
+        item = 0
+        for j in itertools.count(1):
+            sign = -1 if j % 2 == 0 else +1
+            index = (j * j * 3 - j) // 2
+            if index > i:
+                break
+            item += partitions[i - index] * sign
+            index += j  # index == (j * j * 3 + j) // 2
+            if index > i:
+                break
+            item += partitions[i - index] * sign
+            item %= MODULUS
 
+        # Check or memoize the number
+        if item == 0:
+            return str(i)
+        partitions.append(item)
 
-primes = [2]
-def num_prime_sum_ways(n):
-    for i in range(primes[-1] + 1, n + 1):
-        if is_prime(i):
-            primes.append(i)
-
-    ways = [1] + [0] * n
-    for p in primes:
-        for i in range(n + 1 - p):
-            ways[i + p] += ways[i]
-    return ways[n]
 
 if __name__ == "__main__":
     print(compute())
